8bbef30e904eed57d5017850d7fe912d4aaf5fbb
Soupsix <truongnvhe180438@fpt.edu.vn>
2026-01-25 16:45:43 +0700

    setup frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import HomePage from "./pages/HomePage/HomePage";
+import ProductsList from "./pages/ProductsList/ProductsList";
 
 function App() {
   
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/components/layout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const MainLayout = () => {
+    return (
+        <>
+            <Header />
+            <Navbar />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default MainLayout
